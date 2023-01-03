f0301b3c5eded03264f19967062594d200f2f168
NomoteteS <nomotetes.onetrue@icloud.com>
2023-01-03 18:03:20 +0200

    set Architectur&ePackage

---
+++ SnapKit App/Features/SnapKit/View/SnapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SnapViewController.swift
+//  SnapKit App
+//
+//  Created by NomoteteS on 3.01.2023.
+//
+
+import UIKit
+
+final class SnapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
